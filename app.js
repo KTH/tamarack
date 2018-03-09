13a58516244edf80fb9c1f02eba3db20b10c173d
Patric Jansson <paddy@kth.se>
2018-03-09 15:03:43 +0100

    Access logging added.

--- app.js
+++ app.js
@@ -29,21 +29,33 @@ log.init({
 });
 
 app.get("/", function(req, res) {
+  app.logRequest(req);
   res.status(200).send(templates.index());
 });
 
 app.get("/_about", function(req, res) {
+  app.logRequest(req);
   res.status(200).send(templates._about());
 });
 
 app.get("/_monitor", function(req, res) {
+  app.logRequest(req);
   res.set("Content-Type", "text/plain");
   res.status(200).send(templates._monitor());
 });
 
 app.use(function(req, res) {
+  app.logRequest(req, 404);
   res.status(404).send(templates.error404());
 });
+
+app.logRequest = function(req, statusCode = 200) {
+  log.info(
+    `${req.headers["x-forwarded-for"]} ${statusCode} ${req.get("Host")}/${
+      req.url
+    }`
+  );
+};
 
 /**
  * Start server on package.json port or default to 80.
