d7dce518cb051421df87c6696757f7b6513f6ddc
Patric Jansson <paddy@kth.se>
2018-03-09 12:59:45 +0100

    doc change

--- app.js
+++ app.js
@@ -1,57 +1,66 @@
-var appInsights = require('applicationinsights');
-const express = require('express');
+var appInsights = require("applicationinsights");
+const express = require("express");
 const app = express();
-const log = require('kth-node-log')
-const templates = require('./modules/templates')
-const about = require('./config/version');
-const os = require('os');
-const packageFile = require('./package.json')
+const log = require("kth-node-log");
+const templates = require("./modules/templates");
+const about = require("./config/version");
+const os = require("os");
+const packageFile = require("./package.json");
 
-appInsights.setup().setAutoDependencyCorrelation(true)
-.setAutoCollectRequests(true)
-.setAutoCollectPerformance(true)
-.setAutoCollectExceptions(true)
-.setAutoCollectDependencies(true)
-.setAutoCollectConsole(true)
-.setUseDiskRetryCaching(true)
-.start();
-
+appInsights
+  .setup()
+  .setAutoDependencyCorrelation(true)
+  .setAutoCollectRequests(true)
+  .setAutoCollectPerformance(true)
+  .setAutoCollectExceptions(true)
+  .setAutoCollectDependencies(true)
+  .setAutoCollectConsole(true)
+  .setUseDiskRetryCaching(true)
+  .start();
 
 log.init({
-    name: packageFile.name,
-    app: packageFile.name,
-    env: 'dev',
-    level: 'DEBUG',
-    console: false,
-    stdout: true,
-    src: true
-})
-
-app.get('/', function (req, res) {
-    res.status(200).send(templates.index())
+  name: packageFile.name,
+  app: packageFile.name,
+  env: "dev",
+  level: "DEBUG",
+  console: false,
+  stdout: true,
+  src: true
 });
 
-app.get('/_about', function (req, res) {
-    res.status(200).send(templates._about())
+app.get("/", function(req, res) {
+  res.status(200).send(templates.index());
 });
 
-app.get('/_monitor', function (req, res) {
-    res.set("Content-Type", "text/plain");
-    res.status(200).send(templates._monitor())
+app.get("/_about", function(req, res) {
+  res.status(200).send(templates._about());
 });
 
-app.use(function (req, res) {
-    res.status(404).send(templates.error404())
+app.get("/_monitor", function(req, res) {
+  res.set("Content-Type", "text/plain");
+  res.status(200).send(templates._monitor());
+});
+
+app.use(function(req, res) {
+  res.status(404).send(templates.error404());
 });
 
 /**
- * Start server on package.json port or default to 3000.
+ * Start server on package.json port or default to 80.
  */
-app.getListenPort = function () {
-    return process.env.PORT ? process.env.PORT : 80;
+app.getListenPort = function() {
+  return process.env.PORT ? process.env.PORT : 80;
 };
 
-app.listen(app.getListenPort(), function () {
-    console.log(`${packageFile.name} running on port ${app.getListenPort()} on ${os.hostname()}`);
-    log.info(`Using Application Ingsights: ${process.env.APPINSIGHTS_INSTRUMENTATIONKEY ? "yes" : "no"}.`)
+app.listen(app.getListenPort(), function() {
+  console.log(
+    `${
+      packageFile.name
+    } running on port ${app.getListenPort()} on ${os.hostname()}`
+  );
+  log.info(
+    `Using Application Ingsights: ${
+      process.env.APPINSIGHTS_INSTRUMENTATIONKEY ? "yes" : "no"
+    }.`
+  );
 });
