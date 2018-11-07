b506bd04240d5df909aa096e1c7007b8655089ad
Patric Jansson <paddy@kth.se>
2018-11-07 16:38:18 +0100

    Better label

--- modules/templates.js
+++ modules/templates.js
@@ -1,13 +1,13 @@
-'use strict'
+"use strict";
 
-const os = require('os');
-const about = require('./../config/version');
+const os = require("os");
+const about = require("./../config/version");
 
 /**
  * Header html
  */
 let header = function header(title) {
-    return `<!DOCTYPE html>
+  return `<!DOCTYPE html>
     <!-- Served by Tamarack -->
     <html lang="en">
     <title>${title}</title>
@@ -35,14 +35,14 @@ let header = function header(title) {
 
         <div id="flottsbro-error-message"></div>
         <div id="flottsbro-info-message"></div>
-`
-}
+`;
+};
 
 /**
  * Footer html
  */
 let footer = function footer(code) {
-    return `
+  return `
             <p>You probably want to go to <a href="https://www.kth.se">www.kth.se</a>.</p>
             <p>For current application status, please see our <a href="https://www.kthstatus.se/">status page</a>.</p>
             <p style="font-size: 0.6rem; color: #666">Page served by: Tamarack</p>
@@ -51,26 +51,26 @@ let footer = function footer(code) {
 
     </body>
     </html>
-`
-}
+`;
+};
 
 /**
  * 404 error page
  */
 let error404 = function error404() {
-    return `
+  return `
     ${header("404 - Page not found")}
     <h1 style="font-size: 1.8rem; font-family: Georgia Regular,Georgia,garamond pro,garamond,times new roman,times,serif; font-weight: 400;">No service matched your requested page</h1>
     <p>The service may have been moved or deleted. Please also check the web address for proper spelling and capitalization.</p>
     ${footer("404")}
     `;
-}
+};
 
 /**
  * Index page.
  */
 let index = function index() {
-    return `
+  return `
     ${header("KTH Applications")}
             <h1 style="font-size: 1.8rem; font-family: Georgia Regular,Georgia,garamond pro,garamond,times new roman,times,serif; font-weight: 400;">KTH Applications </h1>
             <p>You probably want to go to <a href="https://www.kth.se">www.kth.se</a>.</p>
@@ -81,45 +81,48 @@ let index = function index() {
     </body>
     </html>
     `;
-
-}
+};
 
 /**
  * robots.txt
  */
 let _robotstxt = function robotstxt() {
-    return `User-agent: *\nDisallow: /`
-}
+  return `User-agent: *\nDisallow: /`;
+};
 
 /**
  * Monitor page
  */
 let _monitor = function _monitor() {
-    return `APPLICATION_STATUS: OK\nCLUSTER: ${(process.env.PORTILLO_CLUSTER ? process.env.PORTILLO_CLUSTER : "")}`
-}
+  return `APPLICATION_STATUS: OK\nCLUSTER: ${
+    process.env.PORTILLO_CLUSTER ? process.env.PORTILLO_CLUSTER : ""
+  }`;
+};
 
 /**
  * About page
  */
 let _about = function _about() {
-    return `
+  return `
     ${header("KTH Applications")}
-            <p><strong>Docker image:</strong> ${about.dockerName}:${about.dockerVersion}</p>
+            <p><strong>Docker image:</strong> ${about.dockerName}:${
+    about.dockerVersion
+  }</p>
             <p><strong>Hostname:</strong> ${os.hostname()}</p>
-            <p><strong>Jenkins build date:</strong> ${about.jenkinsBuildDate}</p>
+            <p><strong>Build date:</strong> ${about.jenkinsBuildDate}</p>
         </div>
     </body>
     </html>
-    `
-}
+    `;
+};
 
 /**
  * Module exports
  */
 module.exports = {
-    index: index,
-    error404: error404,
-    _monitor: _monitor,
-    _about: _about,
-    robotstxt: _robotstxt
-}
+  index: index,
+  error404: error404,
+  _monitor: _monitor,
+  _about: _about,
+  robotstxt: _robotstxt
+};
