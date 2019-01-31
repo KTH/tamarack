43e43ea5366df90150ad8dd4f472f35b2c9da17b
Patric Jansson <paddy@kth.se>
2019-01-31 09:56:50 +0100

    Removed user-scalable=yes

--- modules/templates.js
+++ modules/templates.js
@@ -11,7 +11,7 @@ let header = function header(title) {
     <!-- Served by Tamarack -->
     <html lang="en">
     <title>${title}</title>
-    <meta name="viewport" content="width=device-width,initial-scale=1.0,shrink-to-fit=no,user-scalable=yes">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,shrink-to-fit=no">
     <script>
     var appInsights=window.appInsights||function(a){
         function b(a){c[a]=function(){var b=arguments;c.queue.push(function(){c[a].apply(c,b)})}}var c={config:a},d=document,e=window;setTimeout(function(){var b=d.createElement("script");b.src=a.url||"https://az416426.vo.msecnd.net/scripts/a/ai.0.js",d.getElementsByTagName("script")[0].parentNode.appendChild(b)});try{c.cookie=d.cookie}catch(a){}c.queue=[];for(var f=["Event","Exception","Metric","PageView","Trace","Dependency"];f.length;)b("track"+f.pop());if(b("setAuthenticatedUserContext"),b("clearAuthenticatedUserContext"),b("startTrackEvent"),b("stopTrackEvent"),b("startTrackPage"),b("stopTrackPage"),b("flush"),!a.disableExceptionTracking){f="onerror",b("_"+f);var g=e[f];e[f]=function(a,b,d,e,h){var i=g&&g(a,b,d,e,h);return!0!==i&&c["_"+f](a,b,d,e,h),i}}return c
