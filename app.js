8737771830641951d5cfcad8ee47588f2ca23d96
Patric Jansson <paddy@kth.se>
2019-03-06 15:07:22 +0100

    Still looking for status code

--- app.js
+++ app.js
@@ -155,6 +155,8 @@ app.get("/_application", function (request, response) {
  * Includes application information route /_application.
  */
 app.get("/error5xx.html", function (request, response) {
+  log.logger.index(request)
+  log.logger.index(request.headers)
   httpResponse.badGateway(request, response, templates.error5xx(request));
 });
 
